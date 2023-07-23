db172e6bc33a42ebc455dcdb2598308f1764cf8e
Tsaplin Mikhail <caplin.m@inbox.ru>
2023-07-24 07:21:31 +1000

    Создание файла Main.py

---
+++ task_3/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        PersonalInformation personal_info = new PersonalInformation();
+        personal_info.Saving_text_file();
+
+    }
+}
